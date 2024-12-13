6296a098943e69761ae2ab7d3a4d47397ce556a1
Gustavo <guto050506@gmail.com>
2024-12-13 16:50:05 -0300

    hangman_game.c

--- hangman.c
+++ hangman.c
@@ -23,26 +23,68 @@ void jogo(const char *palavra_secreta){
     resposta[tam] = '\0';
 
     char chute;
-    int acertos = 0;
     bool letra_rept;
-
-    printf("\nA palavra secreta tem %d letras\n", tam);
+    int max_tent = 10;
+    int num_tent = 0;
 
     while (true){
 
+        system("cls");
+        printf("\nA palavra secreta tem %d letras\n=================================", tam);
+        printf("\n\tPalavra: ");
         for(int i = 0; i < tam; i++){
             printf("%c ", resposta[i]);
         }
 
-        printf("Digite um letra que ache que está na palavra: ");
+        printf("\n=======================================");
+        printf("\nVoce tem %d tentivas", max_tent - num_tent);
+        
+        printf("\nDigite uma letra que pode estar na palavra (Digite '1' caso deseja escrevar a palavra): ");
         scanf(" %c", &chute);
 
-        for(int i = 0; i < tam; i++){
-            if(chute == palavra_secreta[i]){
-                if(resposta[i] == '_'){
-                    resposta[i] = chute;
+        if(chute == '1'){
+            char palavra_tentativa[60]; // Variável separada
+            printf("\nDigite a palavra completa: ");
+            scanf(" %s", palavra_tentativa);
+
+            if(strcmp(palavra_secreta, palavra_tentativa) == 0){
+                printf("\nParabens!! Voce acertou a palavra secreta!");
+                printf("\n");
+                system("pause");
+                break;
+            } else {
+                printf("\nErrou! A palavra secreta era: %s", palavra_secreta);
+                printf("\n");
+                system("pause");
+                break;
+            }
+
+        }else{
+            
+            letra_rept = true;
+            for(int i = 0; i < tam; i++){
+                if(chute == palavra_secreta[i]){
+                    if(resposta[i] == '_'){
+                        resposta[i] = chute;
+                        letra_rept = false;
+                    }
                 }
             }
+            
+            if(letra_rept){
+                printf("\nEssa letra nao tem na palavra ou ja foi usada.\n");
+                printf("\n");
+                system("pause");
+            }
+        }
+
+        num_tent++;
+
+        if(num_tent == max_tent){
+            printf("\nFim de Jogo! Voce atingiu o maximo de tentativas");
+            printf("\n");
+            system("pause");
+            break;
         }
 
         if (strcmp(palavra_secreta, resposta) == 0){
@@ -52,8 +94,8 @@ void jogo(const char *palavra_secreta){
             break;
         }
     }
-    
 }
+
 void Computador_HangmanGame(){
 
 }
@@ -71,7 +113,7 @@ int main(){
         }else if(strcmp(escolha, "1") == 0){
 
             char palavra_secreta[60];
-            printf("\n===== Jogador 1 =====");
+            printf("\n========= Jogador 1 =========");
             printf("\nDigite a Palavra a ser Adivinhada: ");
             scanf(" %s", palavra_secreta);
             jogo(palavra_secreta);
